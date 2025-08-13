cdef659bde17286e619a4ed3aa6319505a06c152
Fluqz <robin@hlawinka.de>
2025-08-14 01:39:24 +0200

    removing js

--- core/components/tag-component.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TagComponent = void 0;
-class TagComponent {
-    constructor(tagName) {
-        tagName = tagName;
-    }
-}
-exports.TagComponent = TagComponent;
